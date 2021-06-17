d06ef27427e942de0783a0056e0c328ca5727257
Johnlouis Dahhan <47329820+jdahhan@users.noreply.github.com>
2021-06-17 00:20:56 -0400

    Create module1.py

---
+++ module1.py
@@ -0,0 +1,51 @@
+#%% imports & initialization
+import requests
+import pubmed_parser as pp
+import pandas as pd
+import gzip
+from nltk import tokenize
+
+SIZE = 2 # how many files are we bothering with
+parsed_files = []
+fnames = ["pubmed21n" + str(num).zfill(4) + ".xml" for num in range(1000,1000+SIZE + 1)]
+
+#%% getting + processing files
+ftp_url = "https://ftp.ncbi.nlm.nih.gov/pubmed/baseline/"
+for fname in fnames:
+    r = requests.get(ftp_url + fname + '.gz')
+    print('yoinked')
+    medline_data = pd.DataFrame.from_dict(pp.parse_medline_xml(gzip.decompress(r.content)))
+    medline_data = medline_data[['abstract', 'pubdate']]
+    parsed_files.append(medline_data[:50])
+    print('parsed')
+
+#%% create dataframe of parsed files
+pubmed_df = pd.concat(parsed_files)
+print(pubmed_df['pubdate'][10])
+#%% isolate sentences
+#pull abstracts <= 2015
+abstracts = [pubmed_df['abstract'][i] for i in pubmed_df.index if int(pubmed_df['pubdate'][i]) <= 2015]
+sentences = []
+for abstract in abstracts:
+    sentences += tokenize.sent_tokenize(abstract)
+
+#%% narrow sentences down to only those with drug-gene relationships
+usable_sentences = []
+#NO IDEA WHERE TO GET THESE
+"""
+drugs = []
+genes = []
+
+for sentence in sentences:
+    token_sentence = set(tokenize.tokenize(sentence))
+    for drug in drugs:
+        if drug in sentence:
+            for gene in genes:
+                if gene in sentence:
+                    usable_sentences.append(sentence)
+"""
+#%% writing final sentences
+file = open("usable_sentences.tsv", "w")
+file.write('\t'.join(sentences))
+file.close()
+print('done')
